1440d77d0bcd993f3d54b224aae3e00c274171c1
selivura1 <selivura@list.ru>
2023-11-03 15:40:33 +0500

    Переделал второго босса.

--- Assets/Source/Scripts/Entities/Enemies/Bosses/Demonica/DemonicaBossAI.cs
+++ Assets/Source/Scripts/Entities/Enemies/Bosses/Demonica/DemonicaBossAI.cs
@@ -6,16 +6,43 @@ namespace Ultracat
         private EntityBase _entity;
         private PlayerEntity _player;
         private Movement _movement;
-        [SerializeField] private float _zoneAttackRange = 3;
-        [SerializeField] private float _zoneAttackDamageMultiplier = 2;
-        [SerializeField] private int _zoneAttackAmount = 3;
-        [SerializeField] private Projectile _zoneAttack;
+        private Animator _animator;
+
+        [Header("Explosion")]
+        [SerializeField] private Projectile _explosionAttackProjectile;
+        [SerializeField] private AudioClip _explosionSpawnSound;
+        
+
+        [Header("Explosion attack")]
+        [SerializeField] private float _explosionAttackRange = 3;
+        [SerializeField] private float _explosionAttackDamageMultiplier = 2;
+
+        [Header("Big explosion attack")]
+        [SerializeField] string _bigExplosionAttack = "BigExplosionAttack";
+        [SerializeField] private int _bigExplosionAttackAmount = 6;
+
+        [Header("Melee attack")]
+        [SerializeField] string _roundMeleeAttack = "RoundMeleeAttack";
         [SerializeField] private Projectile _meleeAttackProjectile;
-        [SerializeField] private Projectile _rangeAttackProjectile;
-        [SerializeField] private AudioClip _attackPrepareSound;
-        [SerializeField] private AudioClip _zoneSpawnSound;
-        [SerializeField] private AudioClip _rangedAttackSound;
         [SerializeField] private AudioClip _meleeAttackSound;
+        [SerializeField] private AudioClip _attackPrepareSound;
+
+        [Header("Leg Melee attack")]
+        [SerializeField] string _legMeleeAttack = "LegMeleeAttack";
+
+        [Header("Punch melee attack")]
+        [SerializeField] string _punchMeleeAttack = "PunchMeleeAttack";
+
+        [Header("Barrage attack")]
+        [SerializeField] string _barrageAttack = "Barrage";
+
+        [Header("Fireball attack")]
+        [SerializeField] string _fireballAttack = "FireballAttack";
+        [SerializeField] float _fireballDamageMultiplier = 1;
+        [SerializeField] private Projectile _rangeAttackProjectile;
+        [SerializeField] private AudioClip _rangedAttackSound;
+
+        int _combo = 0;
         private GlobalSoundSpawner _soundSpawner;
         private bool _moveToPlayer;
         private void Start()
@@ -24,6 +51,7 @@ namespace Ultracat
             _movement = GetComponent<Movement>();
             _player = FindAnyObjectByType<PlayerEntity>();
             _entity = GetComponent<EntityBase>();
+            _animator = GetComponent<Animator>();
         }
         private void FixedUpdate()
         {
@@ -36,45 +64,114 @@ namespace Ultracat
                 _movement.Move(Vector2.zero, 0);
             }
         }
-        public void AttackMelee()
+        public void StartAttackByCombo()
+        {
+            switch (_combo)
+            {
+                case 0:
+                    PerformAttack(_barrageAttack);
+                    break;
+                case 1:
+                    PerformAttack(_bigExplosionAttack);
+                    break;
+                case 2:
+                    PerformAttack(_barrageAttack);
+                    break;
+                case 3:
+                    PerformAttack(_fireballAttack);
+                    break;
+                case 4:
+                    PerformAttack(_punchMeleeAttack);
+                    break;
+                case 5:
+                    PerformAttack(_bigExplosionAttack);
+                    break;
+                case 6:
+                    PerformAttack(_roundMeleeAttack);
+                    _combo = 0;
+                    break;
+                default:
+                    PerformAttack(_fireballAttack);
+                    break;
+            }
+        }
+        public void SpawnCrossExplosionPrison()
+        {
+            //X0X
+            //0X0
+            //X0X
+            int size = 2;
+            Vector3 offset = new Vector3(-1, 1);
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if(!((y == 0 && x == 1) || (y == 1 && x == 2) || (y == 1 && x == 0) || (y == 2 && x == 1)))
+                    SpawnExplosion(_player.transform.position + new Vector3(x * size, -y * size) + offset * size);
+                }
+            }
+        }
+        public void SpawnPlusExplosionPrison()
+        {
+            //0X0
+            //XXX
+            //0X0
+            int size = 2;
+            Vector3 offset = new Vector3(-1, 1);
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (!((y == 0 && x == 0) || (y == 0 && x == 2) || (y == 2 && x == 0) || (y == 2 && x == 2)))
+                        SpawnExplosion(_player.transform.position + new Vector3(x * size, -y * size) + offset * size);
+                }
+            }
+        }
+        private void PerformAttack(string attackKey)
+        {
+            _animator.Play(attackKey, 0);
+            _combo++;
+        }
+
+        public void AttackRoundMelee()
         {
             _soundSpawner.PlaySound(_meleeAttackSound, SoundType.Enemy);
             _moveToPlayer = false;
             var spawned = GameManager.AttacksPool.Get(_meleeAttackProjectile);
             spawned.transform.position = transform.position;
-            spawned.Initialize(_entity, _player.transform.position - transform.position, _entity.EntityStats.Attack.Value * _zoneAttackDamageMultiplier);
+            spawned.Initialize(_entity, _player.transform.position - transform.position, _entity.EntityStats.Attack.Value);
         }
-        public void AttackRange()
+        public void ShootFireballAtTarget()
         {
             _soundSpawner.PlaySound(_rangedAttackSound, SoundType.Enemy);
             _moveToPlayer = false;
             var spawned = GameManager.AttacksPool.Get(_rangeAttackProjectile);
             spawned.transform.position = transform.position;
-            spawned.Initialize(_entity, _player.transform.position - transform.position, _entity.EntityStats.Attack.Value * _zoneAttackDamageMultiplier);
+            spawned.Initialize(_entity, _player.transform.position - transform.position, _entity.EntityStats.Attack.Value* _fireballDamageMultiplier);
         }
-        public void AttackZoneSnipe()
+        public void SpawnBigExplosionOnTarget()
         {
-            _soundSpawner.PlaySound(_zoneSpawnSound, SoundType.Enemy);
+            _soundSpawner.PlaySound(_explosionSpawnSound, SoundType.Enemy);
             _moveToPlayer = false;
-            var amount = _zoneAttackAmount;
-            SpawnZone(_player.transform.position);
+            var amount = _bigExplosionAttackAmount;
+            SpawnExplosion(_player.transform.position);
             for (int i = 0; i < amount; i++)
             {
-                SpawnZone(_player.transform.position + new Vector3(Random.Range(-_zoneAttackRange, _zoneAttackRange), Random.Range(-_zoneAttackRange, _zoneAttackRange)));
+                SpawnExplosion(_player.transform.position + new Vector3(Random.Range(-_explosionAttackRange, _explosionAttackRange), Random.Range(-_explosionAttackRange, _explosionAttackRange)));
             }
         }
-        public void SpawnZoneOnPlayer()
+        public void SpawnExplosionOnTarget()
         {
-            _soundSpawner.PlaySound(_zoneSpawnSound, SoundType.Enemy);
-            SpawnZone(_player.transform.position);
+            _soundSpawner.PlaySound(_explosionSpawnSound, SoundType.Enemy);
+            SpawnExplosion(_player.transform.position);
         }
-        private void SpawnZone(Vector3 pos)
+        private void SpawnExplosion(Vector3 pos)
         {
-            var spawned = GameManager.AttacksPool.Get(_zoneAttack);
+            var spawned = GameManager.AttacksPool.Get(_explosionAttackProjectile);
             spawned.transform.position = pos;
-            spawned.Initialize(_entity, Vector2.zero, _entity.EntityStats.Attack.Value);
+            spawned.Initialize(_entity, Vector2.zero, _entity.EntityStats.Attack.Value * _explosionAttackDamageMultiplier);
         }
-        public void DashToPlayer()
+        public void DashToTarget()
         {
             _soundSpawner.PlaySound(_attackPrepareSound, SoundType.Enemy, 1, 1);
             _moveToPlayer = false;
@@ -89,5 +186,4 @@ namespace Ultracat
             _moveToPlayer = false;
         }
     }
-
 }
--- Assets/Source/Scripts/Entities/Enemies/Bosses/Demonica/Demonica_run.cs
+++ Assets/Source/Scripts/Entities/Enemies/Bosses/Demonica/Demonica_run.cs
@@ -3,53 +3,15 @@ namespace Ultracat
 {
     public class Demonica_run : StateMachineBehaviour
     {
-        PlayerEntity _player;
-        public float BarrageChance = 25;
-        public float ZoneAttackChance = 50;
-        public float RangedAttackRange = 10;
-        public string TeleportAttack = "Attack1";
-        public string ZoneAttack = "ZoneAttack";
-        public string BarrageAttack = "Barrage";
-        public string RangeAttack = "RangeAttack";
-        public float Stamina = 3;
-        public float StaminaMax = 3;
-        public float StaminaRegen = 1;
-        public float AttackCost = 1;
+       
         string lastAttack;
         //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-        {
-            _player = FindAnyObjectByType<PlayerEntity>();
-        }
+      
 
         //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            var dist = Vector2.Distance(_player.transform.position, animator.transform.position);
-            Stamina += Time.deltaTime;
-            Stamina = Mathf.Clamp(Stamina, 0, StaminaMax);
-            if (Stamina >= AttackCost)
-            {
-                Stamina -= AttackCost;
-                if (Random.Range(0f, 100f) <= BarrageChance)
-                {
-                    Stamina = 0;
-                    lastAttack = BarrageAttack;
-                }
-                else if (Random.Range(0f, 100f) <= ZoneAttackChance)
-                {
-                    lastAttack = ZoneAttack;
-                }
-                else if (dist < RangedAttackRange)
-                {
-                    lastAttack = RangeAttack;
-                }
-                else
-                {
-                    lastAttack = TeleportAttack;
-                }
-            }
-            animator.SetTrigger(lastAttack);
+            
         }
 
         //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
